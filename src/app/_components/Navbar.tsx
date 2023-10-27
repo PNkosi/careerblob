60859aa2c13fff2b3ecbac3c72e1464c5e47fd1b
Perfect Nkois <perfect.coding98@gmail.com>
2023-10-27 18:56:47 +0200

    Update: Hide signin and signup buttons when user is logged in

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,34 +1,47 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 import Logo from "./Logo";
 import Link from "next/link";
 import { navLinks } from "@/lib/components-static-data";
 import MobileMenuTrigger from "./MobileMenuTrigger";
-import clsx from "clsx";
 import Button from "./Button";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = async () => {
+  const user = await currentUser();
   return (
     <nav className="relative  z-50 h-[10dvh] backdrop-blur-sm bg-violet-900/50 flex items-center">
       <div className="container flex items-center justify-between">
         <Logo />
         <div className="hidden md:block text-light-purple">
-            {navLinks.map(({text, href}, index) => (
-                <Link className="px-8 hover:text-cyan-blue transition-all" key={index} href={href}>{text}</Link>
-            ))}
+          {navLinks.map(({ text, href }, index) => (
+            <Link
+              className="px-8 hover:text-cyan-blue transition-all"
+              key={index}
+              href={href}
+            >
+              {text}
+            </Link>
+          ))}
         </div>
         <div className="flex items-center gap-4">
           <UserButton afterSignOutUrl="/" />
-          <Button href="/sign-in" className="bg-cyan-blue">Sing in</Button>
-          <Button href="/sign-up" className="bg-white hidden md:inline-block">Sing up</Button>
+          {user === null && ( // Only show this buttons when the user is not logged in
+            <>
+              <Button href="/sign-in" className="bg-cyan-blue">
+                Sing in
+              </Button>
+              <Button
+                href="/sign-up"
+                className="bg-white hidden md:inline-block"
+              >
+                Sing up
+              </Button>
+            </>
+          )}
           <MobileMenuTrigger />
         </div>
       </div>
     </nav>
   );
 };
-
-
 
 export default Navbar;
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-    return <SignIn />
+    return (
+        <div className="container flex items-center justify-center">
+            <SignIn />
+        </div>
+    )
 }
