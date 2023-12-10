5a126a9f3a4b9d8072f302a692fe42609e0c3aed
Perfect Nkosi <perfect.coding98@gmail.com>
2023-12-10 17:59:27 +0200

    PWA configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { register } = require('module');
+const withPWA = require('next-pwa')
+
+
+const nextConfig = {
+    ...withPWA({
+        dest: 'public',
+        register: true,
+        skipWaiting: true
+    })
+}
 
 module.exports = nextConfig
+
+
+
+module.exports = nextConfig;
